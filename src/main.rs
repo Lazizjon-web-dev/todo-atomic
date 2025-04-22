4297003cf1f0992b8d6f33f5664d47266945766e
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-04-22 17:22:20 +0500

    feat: Implement interactive command line interface for task management

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use rustyline::DefaultEditor;
+use todo_atomic::core::{ToDoList, task::{Task, TaskID}};
+use todo_atomic::store::memory::InMemory;
 fn main() {
-    println!("Hello, world!");
+    let mut rl = DefaultEditor::new().unwrap();
+    loop {
+        let readline = rl.readline(">> ");
+        let to_do_list = ToDoList::new(InMemory::new());
+
+        match readline {
+            Ok(line) if line.trim() == "" => {
+                println!("Empty line, please enter a command");
+                continue;
+            }
+            Ok(line) => {
+                let line = line.trim();
+                match line {
+                    "\\exit" => {
+                        println!("Exiting...");
+                        break;
+                    }
+                    &_ => {
+                        println!("Unknown command: {}", line);
+                    }
+                }
+            }
+            Err(_) => {
+                println!("Error reading line");
+                break;
+            }
+        }
+    }
 }
