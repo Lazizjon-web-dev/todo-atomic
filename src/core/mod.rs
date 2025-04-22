469292336ddc41f4d5e19009bd936d15e0123371
Lazizjon-web-dev <142150917+Lazizjon-web-dev@users.noreply.github.com>
2025-04-22 11:09:53 +0500

    feat: Add ToDoList struct to core module for managing tasks

---
+++ src/core/mod.rs
@@ -0,0 +1,44 @@
+use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
+use crate::store::{Store, memory::InMemory};
+use crate::core::task::{Task, TaskID};
+
+pub mod task;
+
+pub struct ToDoList<S: Store = InMemory> {
+    store: S,
+    id_gen: AtomicUsize,
+}
+
+impl<S: Store> ToDoList<S> {
+    pub fn new(store: S) -> Self {
+        Self {
+            store,
+            id_gen: AtomicUsize::new(0),
+        }
+    }
+    pub fn add(&self, desc: String) -> TaskID {
+        let id = self.id_gen.fetch_add(1, SeqCst);
+        let task = Task {
+            id: TaskID(id),
+            description: desc,
+        };
+        self.store.insert(task).unwrap();
+        TaskID(id)
+    }
+
+    pub fn remove(&self, id: TaskID) -> Result<(), String> {
+        // Check if the task exists before removing it
+        let task = self.store.get(id.clone()).unwrap();
+        if task.is_none() {
+            return Err(format!("Task with ID {} not found", id.0));
+        }
+        // Remove the task from the store
+        self.store.remove(id.clone()).unwrap();
+        // Check if the task was removed successfully
+        let task = self.store.get(id.clone()).unwrap();
+        if task.is_some() {
+            return Err(format!("Failed to remove task with ID {:?}", id.0));
+        }
+        Ok(())
+    }
+}
